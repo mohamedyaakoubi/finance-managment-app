04926f429ac6357dc21fa55bb5e622eed56365b4
Mohamed Yaakoubi <58109706+mohamedyaakoubi@users.noreply.github.com>
2024-07-31 17:52:18 +0100

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@ import '../Styles/Footer.css';
 export const Footer = () => {
     return (
         <footer>
-            All Rights Reserved by <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">Mohamed Yaakoubi </a>
+            All Rights Reserved by <a href="https://www.linkedin.com/in/yaakoubi-mohamed/" target="_blank" rel="noopener noreferrer">Mohamed Yaakoubi </a>
             made within the D4N bootcamp 2024
         </footer>
     );
